dc39c7700768ab344762f9f3da9987c8a2911e5f
RomanceSky <1171039932@qq.com>
2017-11-16 19:56:41 +0800

    __________________________
    Author:Jun
    Time:2017-11-16
    Content:
    1.在app下创建templates文件夹，同时包含base.html
    2.新建app:Video
    _________________________
     On branch jun
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   User/models.py
    	modified:   User/models.pyc
    	new file:   User/templates/.base.html.swp
    	new file:   User/templates/base.html
    	new file:   User/templates/index.html
    	new file:   User/templates/index_login.html
    	new file:   User/templates/login.html
    	modified:   User/views.py
    	modified:   User/views.pyc
    	new file:   Video/__init__.py
    	new file:   Video/admin.py

--- User/models.py
+++ User/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib import auth 
+from django.contrib.auth.models import User
+
+from User.models import *
 
 
 # ziqiang
--- User/views.py
+++ User/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.template import Context
+from django.contrib.auth.models import User
 # Create your views here.
 
 """
 login_index
 """
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, RequestContext
 from django.http import HttpResponse
 
 def Index(request):
-    #return render_to_response('User/login.html',{'p':'1'},context_isinstance=RequestContext(request)
-    return HttpResponse('1')
+    if request.method == 'POST':
+        if  User.objects.get(username = request.POST['username']):
+            return render_to_response('index_login.html')            
+    return render_to_response('login.html',{'p':'1'}, context_instance=RequestContext(request))
+
+    #return HttpResponse('1')
 
 
